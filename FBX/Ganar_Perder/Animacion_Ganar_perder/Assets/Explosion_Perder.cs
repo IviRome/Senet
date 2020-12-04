22471e7793212598f04d3b2e40500f49ff4fcad0
Jocarfl <jcarreresjr@hotmail.com>
2020-12-04 17:03:53 +0100

    explosion ficha

---
+++ FBX/Ganar_Perder/Animacion_Ganar_perder/Assets/Explosion_Perder.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion_Perder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float minForce;
+
+    public float maxForce;
+
+    public float radius;
+
+    public float DestroyDelay;
+
+    void Start()
+    {
+
+        Explode();
+    }
+
+    public void Explode()
+    {
+        foreach(Transform t in transform)
+        {
+            var rb = t.GetComponent<Rigidbody>();
+
+            if(rb != null)
+            {
+                rb.AddExplosionForce(Random.Range(minForce,maxForce),transform.position,radius );
+            }
+            Destroy(t.gameObject, DestroyDelay);
+        }
+        
+    }
+}
