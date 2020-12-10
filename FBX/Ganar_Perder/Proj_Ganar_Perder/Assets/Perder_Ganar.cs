b2d3dc258f73d93cfee2910e9bbe0607b444afc8
Jocarfl <36475567+Jocarfl@users.noreply.github.com>
2020-12-10 02:14:22 +0100

    Anim Ganar

---
+++ FBX/Ganar_Perder/Proj_Ganar_Perder/Assets/Perder_Ganar.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Perder_Ganar : MonoBehaviour
+{
+
+    public GameObject FichaOriginal;
+    public GameObject FichaFracturada;
+    public GameObject explosion;
+
+    public GameObject explosion_luz;
+
+    public Image fade_perder;
+    public Text texto_perder;
+
+    public Image fade_ganar;
+    //public Text texto_ganar;
+
+    private float timer = 0.7f;
+
+    public SlowMotion tok;
+
+    public Animator anim_ganar;
+    private bool esta_animando= false;
+
+    void Start()
+    {
+        anim_ganar = GetComponent<Animator>();
+        texto_perder.canvasRenderer.SetAlpha(0.0f);
+        fade_perder.canvasRenderer.SetAlpha(0.0f);
+        fade_ganar.canvasRenderer.SetAlpha(0.0f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Start_Ganar();
+
+            //Start_Perder();
+
+        }
+    }
+
+    public void Start_Perder()
+    {
+
+        SpawnFichaFracturada();
+        tok.GetComponent<SlowMotion>().toquen = true;
+
+        fade_perder.CrossFadeAlpha(1, 0.5f, false);
+        StartCoroutine(fade_text_perder());
+
+
+    }
+
+    public void Start_Ganar()
+    {
+        tok.GetComponent<SlowMotion>().toquen = true;
+        fade_ganar.CrossFadeAlpha(1, 0.5f, false);
+        anim_ganar.SetBool("esta_animando",true);
+
+        Destroy(Instantiate(explosion_luz, FichaOriginal.transform.position, FichaOriginal.transform.rotation), 0.5f);
+    }
+
+
+    public void SpawnFichaFracturada()
+    {
+        Destroy(FichaOriginal);
+
+
+        GameObject fractObj = Instantiate(FichaFracturada) as GameObject;
+
+        fractObj.GetComponent<Explosion_Perder>().Explode();
+
+        Destroy(Instantiate(explosion, FichaOriginal.transform.position, FichaOriginal.transform.rotation), 0.5f);
+
+
+
+    }
+
+     IEnumerator fade_text_perder()
+    {
+
+        yield return new WaitForSeconds(0.5f);
+        texto_perder.CrossFadeAlpha(1, 0.5f, false);
+      
+    }
+
+    IEnumerator fade_animacion_ganar()
+    {
+
+        yield return new WaitForSeconds(10f);
+        anim_ganar.Play("anim_ganar");
+
+    }
+}
+
+
+    
+        
+       
+
--- FBX/Ganar_Perder/Proj_Ganar_Perder/Assets/SlowMotion.cs
+++ FBX/Ganar_Perder/Proj_Ganar_Perder/Assets/SlowMotion.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class SlowMotion : MonoBehaviour
 {
 
-    public float slowFactor = 0.5f;
+    public float slowFactor = 0.1f;
+
+    public bool toquen;
    
     // Start is called before the first frame update
     void Start()
@@ -16,7 +18,14 @@ public class SlowMotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Time.timeScale = slowFactor;
-        Time.fixedDeltaTime = Time.timeScale * .05f;
+
+        if (toquen==true) {
+
+            Time.timeScale = slowFactor;
+            Time.fixedDeltaTime = Time.timeScale * .05f;
+        }
+
+        
+        
     }
 }
